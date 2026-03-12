9411caea717ae4edbd2312eec7e77ee6fce41d5c
Alexis LECOINTE <alexislecointe@MacBook-Air-de-Alexis.local>
2026-03-12 23:46:36 +0100

    fix: re-add TimerWrapper import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import TimerWrapper from '@/components/ui/TimerWrapper'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import TimerWrapper from '@/components/ui/TimerWrapper'
