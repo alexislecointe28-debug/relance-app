85ac1d44513c468ffd38ba320946ec5ee6a17dfd
Alexis LECOINTE <alexislecointe@MacBook-Air-de-Alexis.local>
2026-03-12 23:48:39 +0100

    fix: layout propre avec TimerWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import TimerWrapper from '@/components/ui/TimerWrapper'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import TimerWrapper from '@/components/ui/TimerWrapper'
 import './globals.css'
+import TimerWrapper from '@/components/ui/TimerWrapper'
 
 const inter = Inter({ 
   subsets: ['latin'],
